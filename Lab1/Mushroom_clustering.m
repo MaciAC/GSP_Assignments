5dcf09a0bbfd80aebb24f9143347d47d007d08b2
MaciAC <macia.ac@gmail.com>
2022-06-25 16:26:27 +0200

    v2

--- Lab1/Mushroom_clustering.m
+++ Lab1/Mushroom_clustering.m
@@ -1,3 +1,4 @@
+%% Data treatment
 data = readtable('agaricus-lepiota.txt','ReadVariableNames',false);
 cats = categorical(data{:,:});
 
@@ -5,27 +6,29 @@ cats = categorical(data{:,:});
 data_clean= removevars(data,{'Var12'});
 
 % check if some '?' is still there
-ismember('?',data_clean{:,:});
+ismember('?',data_clean{:,:})
 
-% categorize features and 
+% categorize features and classes
 data_cats = categorical(data_clean{:,:});
 labels_cat = data_cats(:,1);
 features_cat = data_cats(:,2:width(data_cats));
-%%
+
+%% Categorical to double
 labels = grp2idx(labels_cat);
 features = zeros(size(features_cat));
 for i = 1:width(features_cat)
     features(:,i) = grp2idx(features_cat(:,i));
 end
 
-%%
-similarity_matrix_hamming = pdist2(features, features, 'hamming');
-%%
-hamming_thr = 0.5;
+%% Similarity Matrix
+similarity_matrix_hamming = pdist2(features, features, 'cosine');
 
-similarity_matrix_hamming(similarity_matrix_hamming < hamming_thr) = 0.0;
+%% Threshold
+hamming_thr = 0.1;
 
-%%
+similarity_matrix_hamming(similarity_matrix_hamming < hamming_thr) = 0.1;
+
+%% D matrix, D^(-1) and D^(-1/2)
 D_hamming = zeros(height(features));
 D_hamming_invsqr = zeros(height(features));
 D_hamming_inv = zeros(height(features));
@@ -34,38 +37,29 @@ for i = 1:height(features)
     D_hamming_invsqr(i,i) = 1/sqrt(D_hamming(i,i));
     D_hamming_inv(i,i) = 1/D_hamming(i,i);
 end
-%%
-L_hamming = D_hamming - similarity_matrix_hamming;
 
-Lsn_hamming = D_hamming_invsqr * L_hamming * D_hamming_invsqr;
+%% Eigenvalues and eigenvectores for the type of Laplacian used
+%1 for Laplacian symmetric normalized
+%2 for Laplacian random walk
+%3 for Laplacian unnormalized
+laplacian_type = 2;
 
-Lrw_hamming = D_hamming_inv * L_hamming;
-
-%% Laplacian symmetric normalized
-k = 2;
-[EigVec_hamming, EigVal_hamming] = eig(Lsn_hamming);
-
-%% Laplacian random walk
-k = 2;
-[EigVec_hamming, EigVal_hamming] = eig(Lrw_hamming);
-%% Laplacian unnormalized
-k = 2;
-[EigVec_euclidean, EigVal_euclidean] = eig(L_euclidean);
-[EigVec_hamming, EigVal_hamming] = eig(L_hamming);
-
-%% sort eigenvalues and get 2 smaller
-[D,I] = sort(diag(EigVal_hamming));
-EigVec_hamming = EigVec_hamming(:, I);
-
-[D,I] = sort(diag(EigVal_euclidean));
-EigVec_euclidean = EigVec_euclidean(:, I);
-%%
-idx_euclidean = kmeans(EigVec_euclidean(:,1:2), k);
+switch laplacian_type
+    case 1
+        Lsn_hamming = D_hamming_invsqr * L_hamming * D_hamming_invsqr;
+        [EigVec_hamming, EigVal_hamming] = eigs(Lsn_hamming,2,'smallestabs');
+    case 2
+        Lrw_hamming = D_hamming_inv * L_hamming;
+        [EigVec_hamming, EigVal_hamming] = eigs(Lrw_hamming,2,'smallestabs');
+    case 3
+        L_hamming = D_hamming - similarity_matrix_hamming;
+        [EigVec_hamming, EigVal_hamming] = eigs(L_hamming,2,'smallestabs');
+end
+%% Using k-means
+k=2;
 idx_hamming = kmeans(real(EigVec_hamming(:,1:2)), k);
-%%
-idx_euclidean(idx_euclidean == 1) = 14;
-idx_euclidean(idx_euclidean == 2) = 5;
 
+%%
 idx_hamming(idx_hamming == 1) = 14;
 idx_hamming(idx_hamming == 2) = 5;
 
@@ -73,14 +67,7 @@ idx_hamming(idx_hamming == 2) = 5;
 ok = idx_hamming == labels;
 sum(ok)
 
-ok = idx_euclidean == labels;
-sum(ok)
-
 %%
-figure
-CM_knn_E=confusionmat(labels,idx_euclidean)
-confusionchart(CM_knn_E,'FontSize',20)
-
 figure
 CM_knn_H=confusionmat(labels,idx_hamming)
 confusionchart(CM_knn_H,'FontSize',20)
@@ -94,12 +81,19 @@ scatter(EigVec_hamming(:,1),EigVec_hamming(:,2), 4, labels, 'filled')
 subplot(2,1,2)
 scatter(EigVec_hamming(:,1),EigVec_hamming(:,2), 4, idx_hamming, 'filled')
 
-figure
-colormap winter
+%% Performance
+TP = CM_knn_H(1,1);
+FP = CM_knn_H(1,2);
+FN = CM_knn_H(2,1);
+TN = CM_knn_H(2,2);
+perf.error = (FP+FN)/(TP+FP+TN+FN);
+perf.accuracy = (TP+TN)/(TP+FP+TN+FN);
+perf.precision = TP/(TP+FP);
+perf.recall = TP/(TP+FN);
+perf.specificity = TN/(TN+FP);
+perf.f_score = 2*((precision*recall)/(precision+recall));
 
-subplot(2,1,1)
-scatter(EigVec_euclidean(:,1),EigVec_euclidean(:,2), 4, labels, 'filled')
-subplot(2,1,2)
-scatter(EigVec_euclidean(:,1),EigVec_euclidean(:,2), 4, idx_euclidean, 'filled')
-
-%%
+P_isClass1_ClassifiedAsClass1 = TP/(TP+FP)
+P_isClass2_ClassifiedAsCLass1 = FP/(TP+FP)
+P_isClass1_ClassifiedAsClass2 = FN/(FN+TN)
+P_isClass2_ClassifiedAsClass2 = TN/(FN+TN)
