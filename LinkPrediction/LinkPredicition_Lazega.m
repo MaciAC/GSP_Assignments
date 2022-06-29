bae3c5a053007f631a535cbcf69446ecd7e42b04
MaciAC <macia.ac@gmail.com>
2022-06-29 19:21:07 +0200

    plot graph , compute false alarm and plot rocs

--- LinkPrediction/LinkPredicition_Lazega.m
+++ LinkPrediction/LinkPredicition_Lazega.m
@@ -1,14 +1,15 @@
 %% 1. Reading and cleaning up the data
+close all
+clear all
+clc
 
 T = readtable('LazegaLawyers/ELwork36.dat');
 
 %% 2. Plot the graph
 A = table2array(T);
-[g,nodenums] = binaryImageGraph(A,4);
-xcoor = g.Nodes.x;
-ycoor = size(nodenums,2)-g.Nodes.y; % Flip to proper plot
+G = graph(A);
 figure(1);
-plotImageGraph(g)
+plot(G,'layout','force');
 
 %% 3. Common Neighbors
 
@@ -27,15 +28,35 @@ for i = 1:N_nodes
     end
 end
 
-for i = selected_idx
-    Adjacency_orig(edges(i,1), edges(i,2)) = Common_Neighbors(A(edges(i,1),:), A(edges(i,2),:));
+score_function = 'Common_Neighbors';
+score_function = 'Jaccard_index';
+score_function = 'Addamic_Addar';
+for i = selected_idx    
+    switch score_function
+        case 'Common_Neighbors'
+            thr_ = 1:10;
+            Adjacency_orig(edges(i,1), edges(i,2)) = Common_Neighbors(A(edges(i,1),:), A(edges(i,2),:));
+        case 'Jaccard_index'
+            thr_ = 0:0.1:1;
+            Adjacency_orig(edges(i,1), edges(i,2)) = Jaccard_index(A(edges(i,1),:), A(edges(i,2),:));
+        case 'Addamic_Addar'
+            thr_ = 0:0.2:2;
+            Adjacency_orig(edges(i,1), edges(i,2)) = Addamic_Addar(A(edges(i,1),:), A(edges(i,2),:));
+            Adjacency_orig(Adjacency_orig == Inf) = -Inf;
+            Adjacency_orig(Adjacency_orig == -Inf) = max(max(Adjacency_orig));
+    end
     Adjacency_orig(edges(i,2), edges(i,1)) = Adjacency_orig(edges(i,1), edges(i,2));
 end
 
 %% 4. Probability of detection and false alarm
-figure(1)
 
-for thr = 1:10
+A_roc = zeros(width(selected_idx),10);
+Adjacency_roc = zeros(width(selected_idx),10);
+P_detection = zeros(10,1);
+P_false_alarm = zeros(10,1);
+
+k=1;
+for thr = thr_
 
 Adjacency = Adjacency_orig;
 Adjacency(Adjacency_orig < thr) = 0.0;
@@ -47,16 +68,17 @@ temp(Adjacency > 0) = 1;
 False_Alarm = A - temp;
 n_detected = 0;
 n_expected = 0;
+zero_expected = 0;
 n_false_alarm = 0;
 j=1;
-A_roc = zeros(width(selected_idx),10);
-Adjacency_roc = zeros(width(selected_idx),10);
 
 for i = selected_idx
-    A_roc(j,thr) =  A(edges(i,1), edges(i,2));
-    Adjacency_roc(j,thr) = Adjacency(edges(i,1), edges(i,2));
+    A_roc(j,k) =  A(edges(i,1), edges(i,2));
+    Adjacency_roc(j,k) = Adjacency(edges(i,1), edges(i,2));
     if A(edges(i,1), edges(i,2)) > 0
         n_expected = n_expected + 1;
+    else
+        zero_expected = zero_expected + 1;
     end
     if Detection(edges(i,1), edges(i,2)) > 0
         n_detected = n_detected + 1; 
@@ -67,13 +89,18 @@ for i = selected_idx
     j = j +1;
 end
 
-P_detection = n_detected / n_expected
+P_detection(k,1) = n_detected / n_expected;
 
-P_false_alarm = n_false_alarm / n_expected
-
-plotroc(reshape(A_roc,1,[]), reshape(Adjacency_roc,1,[]))
-hold on
+P_false_alarm(k,1) = n_false_alarm / zero_expected;
+k = k + 1;
 end
+
+figure
+plotroc(A_roc(:,1)', Adjacency_roc(:,1)','Threshold 1',A_roc(:,2)', Adjacency_roc(:,2)','Threshold 2',...
+    A_roc(:,3)', Adjacency_roc(:,3)','Threshold 3',A_roc(:,4)', Adjacency_roc(:,4)','Threshold 4',...
+    A_roc(:,5)', Adjacency_roc(:,5)','Threshold 5',A_roc(:,6)', Adjacency_roc(:,6)','Threshold 6',...
+    A_roc(:,7)', Adjacency_roc(:,7)','Threshold 7',A_roc(:,8)', Adjacency_roc(:,8)','Threshold 8',...
+    A_roc(:,9)', Adjacency_roc(:,9)','Threshold 9',A_roc(:,10)', Adjacency_roc(:,10)','Threshold 10');
 
 %% functions
 function [N] = Common_Neighbors(vec1, vec2)
@@ -87,5 +114,3 @@ end
 function [N] = Addamic_Addar(vec1, vec2)
     N = sum(1/log(sum(vec1 .* vec2)));
 end
-
-
