db418399fca3f83efc033f41cfdd257cdb68d000
MaciAC <macia.ac@gmail.com>
2022-07-01 09:06:46 +0200

    usweather correct shift operator and plot orientation

--- USWeather/USWeather.m
+++ USWeather/USWeather.m
@@ -2,19 +2,25 @@ run('/Users/maciaac/Documents/UPC/2n Semestre/GSP/GSP_Assignments/gspbox/gsp_sta
 %% Reading and visualization of the data
 
 A = load("US Weather Station Data/adjacency.mat").A;
-position = load("US Weather Station Data/position.mat").position_matrix;
+pos = load("US Weather Station Data/position.mat").position_matrix;
 T_Farenheit = load("US Weather Station Data/temp.mat").Y;
 T_Celsius = (T_Farenheit - 32) * 5/9;
 
-%% Generation of an undirected graph
+position = pos;
+position(:,1) = pos(:,2);
+position(:,2) = pos(:,1);
+
+
 figure(1)
 mesh(T_Celsius);
 
+%% Generation of an undirected graph
 figure(2)
 G = gsp_graph(A,position);
+
 gsp_plot_graph(G);
 
-hawaii_idxs = find(position(:,1)  < 24);
+hawaii_idxs = find(position(:,2)  < 24);
 
 A(hawaii_idxs,:) = [];
 A(:,hawaii_idxs) = [];
@@ -31,8 +37,15 @@ G = gsp_graph(A,position);
 gsp_plot_graph(G);
 
 %% Low pass filter
-% shift operator (A)
-[Vec, Val] = eig(A);
+% shift operator (laplacian)
+D = zeros(height(A));
+for i = 1:height(A)
+    D(i,i) = sum(A(i,:));
+end
+
+L = D - A;
+
+[Vec, Val] = eig(L);
 x = T_Celsius(:,1);
 x_ = conj(Vec)'*x;
 
@@ -71,26 +84,22 @@ for d = days
     x = T_Celsius(:,d);
     x_ = conj(Vec)'*x;
     
-    h_lowpass = zeros(height(x_));
-    h_highpass = zeros(height(x_));
+    h_lowpass = zeros(height(x_),1);
+    h_highpass = zeros(height(x_),1);
     for i = 1:height(x_)
-        h_highpass = 1 / (1 - (Val(i,i) - max_eigenval));
-        h_lowpass = 1 / (1 + Val(i,i));
+        h_highpass(i) = 1 / (1 - (Val(i,i) - max_eigenval));
+        h_lowpass(i) = 1 / (1 + Val(i,i));
     end
     y_high = x_ .* h_highpass;
     y_low = x_ .* h_lowpass;
     
-    y_l = conj(Vec)'*y_high;
-    y_h = conj(Vec)'*y_low;
+    y_h = Vec*y_high;
+    y_l = Vec*y_low;
     figure(d);
     G = gsp_graph(A,position);
-    param.climits=[0 1];
-    subplot(1,2,1)
-    gsp_plot_signal(G,y_l,param);
-    subplot(1,2,2)
-    gsp_plot_signal(G,y_h,param);
-
-    
+    %param.climits=[0 1];
+    subplot(211)
+    gsp_plot_signal(G,y_l);
+    subplot(212)
+    gsp_plot_signal(G,y_h);
 end
-
-%%
